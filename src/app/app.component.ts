57c1bfd9e0cd6766e478f20d19fbdf0baebda1bd
Jun <Jun@Junui-MacBook-Pro.local>
2017-02-15 18:16:03 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'rb-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rb works!';
 }
